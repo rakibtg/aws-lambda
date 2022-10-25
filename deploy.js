ae044b9049ac4e0e48401d43ebc8ea3298e77092
rakibtg <rakibtg@gmail.com>
2022-10-26 00:03:53 +0200

    Removed sleep functionalities

--- deploy.js
+++ deploy.js
@@ -1,7 +1,11 @@
 const child_process = require("child_process");
+const { argv } = require("process");
 
 const FUNCTION_NAME = "MetaTagFinder";
 const BUCKET_NAME = "lambda-websitemetatagsgenerator";
+
+const [, , DEPLOY_TARGET] = argv;
+console.log("Deploying:", DEPLOY_TARGET);
 
 const Terminal = (command) => {
   return new Promise((resolve, reject) => {
@@ -16,28 +20,14 @@ const Terminal = (command) => {
   });
 };
 
-const Sleep = (seconds, convertToSeconds = true) => {
-  if (convertToSeconds) {
-    seconds = seconds * 1000;
-  }
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res();
-    }, seconds);
-  });
-};
-
 const Deploy = async () => {
   console.info("⌛ Compressing code");
   await Terminal(
-    `powershell "Compress-Archive .\\MetaTagFinder\\index.js functions.zip"`
+    `powershell "Compress-Archive .\\functions\\${DEPLOY_TARGET}\\index.js functions.zip"`
   );
-  await Sleep(1);
   console.info("⌛ Uploading code to AWS");
   await Terminal(`aws s3 cp ./functions.zip s3://${BUCKET_NAME}`);
-  await Sleep(1);
   await Terminal("rm ./functions.zip");
-  await Sleep(1);
   console.info("⌛ Updating lambda function");
   await Terminal(
     `aws lambda update-function-code --function-name ${FUNCTION_NAME} --s3-bucket ${BUCKET_NAME} --s3-key functions.zip --region eu-north-1`
