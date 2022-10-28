f50c493797881066d74c4123e536a2b485bd857b
rakibtg <rakibtg@gmail.com>
2022-10-28 08:46:07 +0200

    Fixed destination path for uploader

--- functions/MetaTagFinder/utils/Uploader.js
+++ functions/MetaTagFinder/utils/Uploader.js
@@ -1,9 +1,14 @@
 const fs = require("fs");
+const path = require("path");
 const AWS = require("aws-sdk");
 AWS.config.update({ region: "eu-north-1" });
+const isProd = process.env.ENV === "production";
 
 const Uploader = async ({ fileName, bucketName }) => {
-  const fileContent = fs.readFileSync(`./${fileName}`);
+  const dst = isProd
+    ? `/tmp/${fileName}`
+    : path.join(__dirname, "../../..", "tmp", fileName);
+  const fileContent = fs.readFileSync(dst);
   const s3 = new AWS.S3({ apiVersion: "2006-03-01", region: "eu-north-1" });
 
   return s3
