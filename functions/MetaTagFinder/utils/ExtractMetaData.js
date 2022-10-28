098df31fb1e490c28dedd0bef8a79832f3bfb421
rakibtg <rakibtg@gmail.com>
2022-10-28 08:45:12 +0200

    Allow URL fetching in batch

--- functions/MetaTagFinder/utils/ExtractMetaData.js
+++ functions/MetaTagFinder/utils/ExtractMetaData.js
@@ -1,9 +1,19 @@
 const { extractMetadata } = require("link-meta-extractor");
 
 const ExtractMetaData = async ({ url }) => {
-  const metaInformation = await extractMetadata(url);
+  const urlArray = typeof url === "string" ? [url] : [...url];
+  const results = await Promise.all(
+    urlArray.map(async (pageAddress) => {
+      try {
+        const metaInformation = await extractMetadata(pageAddress);
+        return metaInformation;
+      } catch (error) {
+        // log fail.
+      }
+    })
+  );
 
-  return metaInformation;
+  return results;
 };
 
 exports.ExtractMetaData = ExtractMetaData;
